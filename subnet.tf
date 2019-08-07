4bd7e847dbb74a41aabbd9adcd14396cc97c7b06
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-07 15:15:21 -0700

    edited subnet

--- subnet.tf
+++ subnet.tf
@@ -1,23 +1,76 @@
-resource "aws_subnet" "public" {
+//first  public subnet
+resource "aws_subnet" "public1a" {
   vpc_id     = "${aws_vpc.dev.id}"
   cidr_block = "10.0.1.0/24"
 
-  tags = {
-      Name = "${var.Name}.public"
+   tags = {
+      Name = "${var.Name}.public1a"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
+  }
+}
+//Third  public subnet
+resource "aws_subnet" "public1b" {
+  vpc_id     = "${aws_vpc.dev.id}"
+  cidr_block = "10.0.2.0/24"
+
+   tags = {
+      Name = "${var.Name}.public1b"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
+  }
+}
+//Third  public subnet
+resource "aws_subnet" "public1c" {
+  vpc_id     = "${aws_vpc.dev.id}"
+  cidr_block = "10.0.3.0/24"
+
+   tags = {
+      Name = "${var.Name}.public1c"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
+  }
+}
+//first private subnet
+resource "aws_subnet" "private1a" {
+  vpc_id     = "${aws_vpc.dev.id}"
+  cidr_block = "10.0.4.0/24"
+
+   tags = {
+      Name = "${var.Name}.private1a"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
+  }
+}
+//second private subnet
+
+ resource "aws_subnet" "private1b" {
+  vpc_id     = "${aws_vpc.dev.id}"
+  cidr_block = "10.0.5.0/24"
+
+   tags = {
+      Name = "${var.Name}.private1b"
       Env = "${var.Env}"
       Created_by = "${var.Created_by}"
       Dept = "${var.Dept}"
   }
 }
 
-resource "aws_subnet" "private" {
+ // third private subnet
+}
+resource "aws_subnet" "private1c" {
   vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.6.0/24"
 
-  tags = {
-      Name = "${var.Name}.private"
-      Env = "${var.Env}"
-      Created_by = "${var.Created_by}"
-      Dept = "${var.Dept}"
-  }
+   tags = {
+      Name = "${var.Name}.private1c"
+      Env = "${var.Env}"	      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"	      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"	      Dept = "${var.Dept}"
+
+   }
 }
