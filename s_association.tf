849fe3ca2413562f5b722e20b61b74e7b416f4f2
Mustafa Calik <50895577+mcalik77@users.noreply.github.com>
2019-08-07 16:06:50 -0700

    Update s_association.tf

--- s_association.tf
+++ s_association.tf
@@ -1,4 +1,4 @@
 resource "aws_route_table_association" "a" {
-  subnet_id      = "${aws_subnet.public.id}"
+  subnet_id      = ["${aws_subnet.public1a.id}","${aws_subnet.public1b.id}","${aws_subnet.public1c.id}"]
   route_table_id = "${aws_route_table.r.id}"
 }
